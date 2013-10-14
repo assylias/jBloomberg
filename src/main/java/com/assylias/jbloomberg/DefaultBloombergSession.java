3b0fb7d71a608e6a50c2044e389aea7bceb500f0
Yann Le Tallec <ylt@letallec.org>
2013-10-14 21:13:20 +0100

    Removed unused import

--- src/main/java/com/assylias/jbloomberg/DefaultBloombergSession.java
+++ src/main/java/com/assylias/jbloomberg/DefaultBloombergSession.java
@@ -25,7 +25,6 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import static com.assylias.jbloomberg.DefaultBloombergSession.SessionState.*;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
