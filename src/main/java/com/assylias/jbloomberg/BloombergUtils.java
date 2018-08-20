7414e9e28462f2b008deebf76cc2ae8b371ac5d2
jonfreedman <jon.freedman@zoho.com>
2018-08-20 15:33:06 +0100

    missing commmit...

--- src/main/java/com/assylias/jbloomberg/BloombergUtils.java
+++ src/main/java/com/assylias/jbloomberg/BloombergUtils.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
